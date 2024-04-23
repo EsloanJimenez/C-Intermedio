03263e88a6b69b2e1fc30e88a57067c8f0bffb67
ejpservice <ejptheservice@gmail.com>
2024-04-22 20:10:25 -0400

    Introduciendo los valores por el usuario

--- Practica1/Practica1/Program.cs
+++ Practica1/Practica1/Program.cs
@@ -1,18 +1,51 @@
-﻿using Practica1;
+﻿using static System.Console;
+using Practica1;
 using Practica1.ImplementandoPolimorfismo;
 
-//Persona persona = new Persona("Enrique", 30, "Santo Domingo");
+/*
+Persona persona = new Persona("Enrique", 30, "Santo Domingo");
 
-//persona.getInfo();
+persona.getInfo();
 
-//Estudiante estudiante = new Estudiante("Enrique", 30, "Santo Domingo", "Desarrollo De Software", 144358);
+Estudiante estudiante = new Estudiante("Enrique", 30, "Santo Domingo", "Desarrollo De Software", 144358);
 
-//estudiante.getInfo();
+estudiante.getInfo();
+*/
 
-Rectangulo rectangulo = new Rectangulo(8, 3);
-Circulo circulo = new Circulo(7);
-Triangulo triangulo = new Triangulo(9, 11);
+double alturaRectangulo, baseRectangulo, radio, baseTriangulo, alturaTriangulo;
 
-Console.WriteLine($"Área del rectángulo: \t{rectangulo.CalcularArea()}");
-Console.WriteLine($"Área del círculo: \t{circulo.CalcularArea()}");
-Console.WriteLine($"Área del triángulo: \t{triangulo.CalcularArea()}");
+alturaRectangulo = baseRectangulo = radio = baseTriangulo = alturaTriangulo = 0;
+
+try
+{
+    Write("Digite la altura del rectangulo: \t\t");
+    alturaRectangulo = double.Parse(ReadLine());
+
+    Write("Digite la base del rectangulo: \t\t\t");
+    baseRectangulo = double.Parse(ReadLine());
+
+    Write("Digite el valor de radio: \t\t\t");
+    radio = double.Parse(ReadLine());
+
+    Write("Digite el valor de la base del triangulo: \t");
+    baseTriangulo = double.Parse(ReadLine());
+
+    Write("Digite el valor de la altura del triangulo: \t");
+    alturaTriangulo = double.Parse(ReadLine());
+}
+catch (FormatException ex) {
+    WriteLine("El valor introducido no es un valor correcto, porfavor ingrese un valor valido.");
+    WriteLine(ex.Message);
+}
+catch (Exception e)
+{
+    WriteLine(e.ToString());
+}
+
+Rectangulo rectangulo = new Rectangulo(alturaRectangulo, baseRectangulo);
+Circulo circulo = new Circulo(radio);
+Triangulo triangulo = new Triangulo(baseTriangulo, alturaTriangulo); 
+
+WriteLine($"\nÁrea del rectángulo: \t\t\t\t{rectangulo.CalcularArea()}");
+WriteLine($"Área del círculo: \t\t\t\t{circulo.CalcularArea()}");
+WriteLine($"Área del triángulo: \t\t\t\t{triangulo.CalcularArea()}");
