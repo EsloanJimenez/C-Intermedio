bb8c00f0e1cb5b4c4e4e0004d82beddfd4f632a8
ejpservice <ejptheservice@gmail.com>
2024-05-06 19:04:46 -0400

    Pense que lo avia subido :'(

---
+++ Practica2/Practica2/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using static System.Console;
+public class Persona {
+
+    private string nombre;
+    private int edad;
+    private string direccion;
+    private string correoElectronico;
+
+    public void EnviarCorreoElectronico(string mensaje)
+    {
+        // Lógica para enviar correo electrónico
+    }
+
+    public void ImprimirDatos() { 
+        WriteLine($"Nombre: {nombre}");
+        WriteLine($"Edad: {edad}");
+        WriteLine($"Dirección: {direccion}");
+        WriteLine($"Correo electrónico: {correoElectronico}");
+    }
+}
